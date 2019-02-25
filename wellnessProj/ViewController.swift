2b850a98694ee389303bbb206054500e6d4557e8
Jenny Manrique <jennymanrique2411@gmail.com>
2019-02-25 15:50:53 -0800

    Initial Commit

---
+++ wellnessProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wellnessProj
+//
+//  Created by Jenny Manrique on 2019-02-25.
+//  Copyright © 2019 Jenny Manrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
